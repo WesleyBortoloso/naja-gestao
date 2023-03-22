e590be568732428b41aa789078937cc5e409d8b6
Wesley Bortoloso <wesleybortolosocco@gmail.com>
2023-03-22 16:37:39 -0300

    add system styles and manifest to precompile css

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,9 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+//= link home/index.css
+//= link devise/registrations/new.css
+//= link municipes/index.css
+//= link municipes/new.css
+//= link municipes/show.css
